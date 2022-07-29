5a5614d85287648947d63b59e567ea45c3a299f3
KWY0218 <wodrk1001@gmail.com>
2022-07-29 14:33:11 +0900

    #2 [add] add new project

---
+++ app/src/main/java/com/hous/jjbaksa_aos/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.hous.jjbaksa_aos
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
