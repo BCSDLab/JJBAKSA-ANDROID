e06f086b8ec23e5fea9b1583fe66ade1ae38c79d
KWY0218 <wodrk1001@gmail.com>
2022-07-29 16:35:35 +0900

    #2 [mod] modify app gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,12 +11,12 @@ buildscript {
     }
 }
 
-//plugins {
-//    id 'com.android.application' version '7.2.0' apply false
-//    id 'com.android.library' version '7.2.0' apply false
-//    id 'org.jetbrains.kotlin.android' version '1.6.10' apply false
-//    id 'org.jetbrains.kotlin.jvm' version '1.6.10' apply false
-//}
+plugins {
+    id("com.android.application") version "7.2.0" apply false
+    id("com.android.library") version "7.2.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.6.10" apply false
+    id("org.jetbrains.kotlin.jvm") version "1.6.10" apply false
+}
 
 tasks.register("clean", Delete::class) {
     delete(rootProject.buildDir)
