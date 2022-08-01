a7941914f214973ecc815c833f5867894f1b3ec0
KWY0218 <wodrk1001@gmail.com>
2022-08-02 00:05:01 +0900

    #2 [add] add inject dependency in domain module

--- domain/build.gradle.kts
+++ domain/build.gradle.kts
@@ -5,13 +5,14 @@ plugins {
 }
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_1_7
-    targetCompatibility = JavaVersion.VERSION_1_7
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
 }
 
 dependencies {
     KotlinDependencies.run {
         implementation(kotlin)
+        implementation(inject)
     }
 }
 
