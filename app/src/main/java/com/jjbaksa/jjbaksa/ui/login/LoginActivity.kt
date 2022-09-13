a5464397791bd75ab79a50b674c60c7df1329c39
dongmin960903 <dongmin960903@gmail.com>
2022-09-13 23:59:48 +0900

    #4 [ui] ktlint check

--- app/src/main/java/com/jjbaksa/jjbaksa/ui/login/LoginActivity.kt
+++ app/src/main/java/com/jjbaksa/jjbaksa/ui/login/LoginActivity.kt
@@ -25,7 +25,6 @@ class LoginActivity : BaseActivity<ActivityLoginBinding>() {
             textViewSignUp.setOnClickListener {
                 goToSignUpActivity()
             }
-
         }
     }
     fun goToSocialLoginActivity() {
