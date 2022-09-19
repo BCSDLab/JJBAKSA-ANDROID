d213f73ac8e6ee833e642f962b9833c40179cbab
seo <seokw2240@gmail.com>
2022-09-20 05:08:03 +0900

    #5 [chore] - SocialLoginActivity Rename variable

--- app/src/main/java/com/jjbaksa/jjbaksa/ui/social/SocialLoginActivity.kt
+++ app/src/main/java/com/jjbaksa/jjbaksa/ui/social/SocialLoginActivity.kt
@@ -1,72 +1,59 @@
 package com.jjbaksa.jjbaksa.ui.social
 
-import android.app.Activity
 import android.content.Intent
-import android.content.IntentSender
 import android.os.Bundle
-import android.os.PersistableBundle
 import android.util.Log
-import android.util.Xml
-import android.view.View
 import androidx.activity.result.ActivityResultCallback
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.appcompat.app.AppCompatActivity
-import androidx.databinding.DataBindingUtil
-import com.google.android.gms.auth.api.identity.BeginSignInRequest
-import com.google.android.gms.auth.api.identity.Identity
-import com.google.android.gms.auth.api.identity.SignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignIn
-import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions.DEFAULT_SIGN_IN
-import com.google.android.gms.common.SignInButton
 import com.google.android.gms.common.api.ApiException
-import com.google.android.gms.common.api.CommonStatusCodes
 import com.google.firebase.auth.AuthCredential
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.GoogleAuthProvider
-import com.google.firebase.auth.ktx.auth
-import com.google.firebase.ktx.Firebase
 import com.jjbaksa.jjbaksa.R
 import com.jjbaksa.jjbaksa.base.BaseActivity
-import com.jjbaksa.jjbaksa.databinding.ActivityLoginBinding
 import com.jjbaksa.jjbaksa.databinding.ActivitySocialLoginBinding
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
 
 class SocialLoginActivity : BaseActivity<ActivitySocialLoginBinding>() {
     override val layoutId: Int
         get() = R.layout.activity_social_login
 
-    private lateinit var launcher: ActivityResultLauncher<Intent>
+    private lateinit var activityResultLauncher: ActivityResultLauncher<Intent>
     private lateinit var firebaseAuth: FirebaseAuth
-    private var email: String = ""
+    private var userEmail: String = ""
     private var tokenId: String? = null
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        firebaseAuth = FirebaseAuth.getInstance()
-        launcher = registerForActivityResult(
 
+        firebaseAuth = FirebaseAuth.getInstance()
+        activityResultLauncher = registerForActivityResult(
+
+            // Google Auth 로그인 결과 수신
             ActivityResultContracts.StartActivityForResult(), ActivityResultCallback
-            { result ->
-                if (result.resultCode == RESULT_OK) {
-                    val task = GoogleSignIn.getSignedInAccountFromIntent(result.data)
+            { activityResult ->
+                if (activityResult.resultCode == RESULT_OK) {
+                    val task = GoogleSignIn.getSignedInAccountFromIntent(activityResult.data)
                     task.getResult(ApiException::class.java)?.let { account ->
                         tokenId = account.idToken
+                       // Log.d("userToken", tokenId.toString())
+                        //최초접근, 회원가입 구현을 해야하는가? x
                         if (tokenId != null && tokenId != "") {
-                            val credential: AuthCredential =
+                           // Log.d("userToken", tokenId.toString())
+                            val authCredential: AuthCredential =
                                 GoogleAuthProvider.getCredential(account.idToken, null)
-                            firebaseAuth.signInWithCredential(credential)
+                         
+                            firebaseAuth.signInWithCredential(authCredential)
                                 .addOnCompleteListener {
                                     if (firebaseAuth.currentUser != null) {
-                                        val user: FirebaseUser = firebaseAuth.currentUser!!
-                                        email = user.email.toString()
+                                        val firebaseUser: FirebaseUser = firebaseAuth.currentUser!!
+                                        userEmail = firebaseUser.email.toString()
                                         val googleSignInToken = account.idToken ?: ""
+                                        Log.d("userToken", tokenId.toString())
                                     }
                                 }
                         }
@@ -76,22 +63,20 @@ class SocialLoginActivity : BaseActivity<ActivitySocialLoginBinding>() {
 
     }
 
-    override fun onStart() {
-        super.onStart()
+    override fun initView() {
         binding.run {
-            btnGoogleSignIn.setOnClickListener {
-                val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                    .requestIdToken(getString(R.string.default_web_client_id))
-                    .requestEmail()
-                    .build()
-                val googleSignInClient = GoogleSignIn.getClient(this@SocialLoginActivity, gso)
+            btnSocialLoginGoogleSignIn.setOnClickListener {
+                val googleSignInOptions =
+                    GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                        .requestIdToken(getString(R.string.default_web_client_id))
+                        .requestEmail()
+                        .build()
+                val googleSignInClient =
+                    GoogleSignIn.getClient(this@SocialLoginActivity, googleSignInOptions)
                 val signInIntent: Intent = googleSignInClient.signInIntent
-                launcher.launch(signInIntent)
+                activityResultLauncher.launch(signInIntent)
             }
         }
-    }
-
-    override fun initView() {
     }
 
     override fun subscribe() {
