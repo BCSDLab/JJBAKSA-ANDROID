6299cb240f2d23b8c192b641a7fa578675f45020
kongwoojin <kongwoojin03@gmail.com>
2022-10-05 10:31:18 +0900

    #18 [add] Add SignUpUseCase

--- domain/src/main/java/com/jjbaksa/domain/usecase/SignUpUseCase.kt
+++ domain/src/main/java/com/jjbaksa/domain/usecase/SignUpUseCase.kt
@@ -1,10 +1,14 @@
 package com.jjbaksa.domain.usecase
 
 import com.jjbaksa.domain.repository.UserRepository
+import com.jjbaksa.domain.resp.user.SignUpReq
+import com.jjbaksa.domain.resp.user.SignUpResp
 import javax.inject.Inject
 
 class SignUpUseCase @Inject constructor(
     private val userRepository: UserRepository
 ) {
-    operator fun invoke() {}
+    suspend operator fun invoke(signUpReq: SignUpReq): SignUpResp? {
+        return userRepository.postSignUp(signUpReq)
+    }
 }
