45fa83daee2385d4a4b2789864c4461af2d32290
kongwoojin <kongwoojin03@gmail.com>
2022-10-05 10:30:47 +0900

    #18 [add] Add sign up api request

--- data/src/main/java/com/jjbaksa/data/repository/UserRepositoryImpl.kt
+++ data/src/main/java/com/jjbaksa/data/repository/UserRepositoryImpl.kt
@@ -5,14 +5,16 @@ import com.jjbaksa.data.datasource.remote.UserRemoteDataSource
 import com.jjbaksa.data.mapper.CheckAccountAvailableMapper
 import com.jjbaksa.domain.resp.user.SignUpReq
 import com.jjbaksa.domain.repository.UserRepository
+import com.jjbaksa.domain.resp.user.SignUpResp
 import javax.inject.Inject
 
 class UserRepositoryImpl @Inject constructor(
     private val userRemoteDataSource: UserRemoteDataSource,
     private val userLocalDataSource: UserLocalDataSource
 ) : UserRepository {
-    override suspend fun postSignUp(signUpReq: SignUpReq) {
+    override suspend fun postSignUp(signUpReq: SignUpReq): SignUpResp? {
         val resp = userRemoteDataSource.postSignUp(signUpReq)
+        return resp.body()
     }
 
     override suspend fun checkIdAvailable(account: String): Boolean {
--- domain/src/main/java/com/jjbaksa/domain/repository/UserRepository.kt
+++ domain/src/main/java/com/jjbaksa/domain/repository/UserRepository.kt
@@ -1,8 +1,9 @@
 package com.jjbaksa.domain.repository
 
 import com.jjbaksa.domain.resp.user.SignUpReq
+import com.jjbaksa.domain.resp.user.SignUpResp
 
 interface UserRepository {
-    suspend fun postSignUp(signUpReq: SignUpReq)
+    suspend fun postSignUp(signUpReq: SignUpReq): SignUpResp?
     suspend fun checkIdAvailable(account: String): Boolean
 }
