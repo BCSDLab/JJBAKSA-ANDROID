c622c7c32ee71d133af7841cfa2ab8712566cfa8
kongwoojin <kongwoojin03@gmail.com>
2022-10-05 10:38:24 +0900

    #18 [chore] Fix ktlint

--- app/src/main/java/com/jjbaksa/jjbaksa/ui/signup/SignUpFragment.kt
+++ app/src/main/java/com/jjbaksa/jjbaksa/ui/signup/SignUpFragment.kt
@@ -7,7 +7,6 @@ import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
-import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import com.jjbaksa.domain.enums.SignUpAlertEnum
 import com.jjbaksa.domain.enums.SignUpAlertEnum.EMAIL_NOT_FOUND
