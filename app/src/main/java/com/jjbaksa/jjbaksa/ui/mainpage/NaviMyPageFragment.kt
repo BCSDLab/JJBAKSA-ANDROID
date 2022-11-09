b356b156df96b665d55c5fd9dc4740afbd0576da
임성우 <seongwoorang@naver.com>
2022-11-09 09:59:23 +0900

    #23 [mod] Modify code convention

--- app/src/main/java/com/jjbaksa/jjbaksa/ui/mainpage/NaviMyPageFragment.kt
+++ app/src/main/java/com/jjbaksa/jjbaksa/ui/mainpage/NaviMyPageFragment.kt
@@ -7,7 +7,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.jjbaksa.jjbaksa.R
 
-class NaviMyPageFragment: Fragment() {
+class NaviMyPageFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
