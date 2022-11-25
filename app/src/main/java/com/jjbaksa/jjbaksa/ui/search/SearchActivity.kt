9f8dbff2e4e916a148999ce6e8bf8f268d8524ef
kongwoojin <kongwoojin03@gmail.com>
2022-11-25 20:06:36 +0900

    #28 [add] Add location permission request

--- app/src/main/java/com/jjbaksa/jjbaksa/ui/search/SearchActivity.kt
+++ app/src/main/java/com/jjbaksa/jjbaksa/ui/search/SearchActivity.kt
@@ -22,12 +22,39 @@ class SearchActivity : BaseActivity<ActivitySearchBinding>() {
     private val searchViewModel: SearchViewModel by viewModels()
     private val searchMainViewModel: SearchMainViewModel by viewModels()
 
+    private val requestPermission =
+        registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { results ->
+            for (result in results) {
+                when (result.value) {
+                    true -> {
+                        // Get location
+                    }
+                    else -> {
+                        when (
+                            shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_FINE_LOCATION) ||
+                                shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_COARSE_LOCATION)
+                        ) {
+                            true -> permissionDialog(true)
+                            else -> permissionDialog(false)
+                        }
+                        break
+                    }
+                }
+            }
+        }
+
+    private val openSettings =
+        registerForActivityResult(OpenSettings()) {
+            checkPermission()
+        }
+
     override fun initView() {
         binding.lifecycleOwner = this
         binding.toolbarSearchTitle.setNavigationIcon(R.drawable.ic_toolbar_back)
         setSupportActionBar(binding.toolbarSearchTitle)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
+        checkPermission()
     }
 
     override fun subscribe() {
@@ -37,6 +64,38 @@ class SearchActivity : BaseActivity<ActivitySearchBinding>() {
     }
 
     override fun initEvent() {
+    }
+
+    private fun checkPermission() {
+        val permissionList = arrayOf(
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.ACCESS_COARSE_LOCATION
+        )
+
+        requestPermission.launch(permissionList)
+    }
+
+    private fun permissionDialog(isDeniedOnce: Boolean) {
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle(getString(R.string.search_ask_locate_permission_dialog_title))
+            .setMessage(getString(R.string.search_ask_locate_permission_dialog_message))
+            .setPositiveButton(getString(R.string.search_ask_locate_permission_dialog_ok)) { dialog, _ ->
+                when (isDeniedOnce) {
+                    true -> checkPermission()
+                    false -> {
+                        openSettings.launch(null)
+                        Toast.makeText(this, getString(R.string.search_ask_locate_permission_setting), Toast.LENGTH_SHORT).show()
+                    }
+                }
+                dialog.dismiss()
+            }
+            .setNegativeButton(getString(R.string.search_ask_locate_permission_dialog_cancel)) { dialog, _ ->
+                dialog.dismiss()
+                Toast.makeText(this, getString(R.string.search_need_permission_toast_message), Toast.LENGTH_SHORT).show()
+                finish()
+            }
+            .setCancelable(false)
+        builder.show()
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
