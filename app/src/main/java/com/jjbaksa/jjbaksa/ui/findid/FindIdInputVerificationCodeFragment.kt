c64c90ee92892b1e9d9581e566cf9bb10719a0e4
Jokwanhee <jonotch1@naver.com>
2023-01-10 20:30:47 +0900

    #25 [mod] modify FindIdInputVerificationCode

--- app/src/main/java/com/jjbaksa/jjbaksa/ui/findid/FindIdInputVerificationCodeFragment.kt
+++ app/src/main/java/com/jjbaksa/jjbaksa/ui/findid/FindIdInputVerificationCodeFragment.kt
@@ -4,15 +4,23 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.EditText
+import android.widget.Toast
 import androidx.core.widget.addTextChangedListener
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.Observer
 import com.jjbaksa.jjbaksa.R
 import com.jjbaksa.jjbaksa.databinding.FragmentFindIdInputVerificationCodeBinding
+import com.jjbaksa.jjbaksa.viewmodel.FindIdViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class FindIdInputVerificationCodeFragment: Fragment() {
     private lateinit var binding: FragmentFindIdInputVerificationCodeBinding
+
+    private val findIdViewModel: FindIdViewModel by activityViewModels()
+    var numberBoxState = mutableListOf<Boolean>(false,false,false,false)
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -21,17 +29,65 @@ class FindIdInputVerificationCodeFragment: Fragment() {
     ): View? {
         binding = DataBindingUtil.inflate(layoutInflater, R.layout.fragment_find_id_input_verification_code, container, false)
 
-        moveNextCodeBox()
+        numberBoxState = mutableListOf(false,false,false,false)
+
+        nextToVerificationCodeBox()
+        observeData()
+
+        binding.textViewFindIdResendVerificationCode.setOnClickListener {
+            Toast.makeText(requireContext(), "인증번호가 재발송 되었습니다.", Toast.LENGTH_SHORT).show()
+            findIdViewModel.getFindIdNumberCode(
+                findIdViewModel.userEmail.value.toString(),
+                null,
+                null
+            )
+        }
+
+        binding.buttonFindIdVerificationCode.setOnClickListener {
+            findIdViewModel.getFindId(
+                binding.editTextFindIdVerificationCodeOne.text.toString(),
+                binding.editTextFindIdVerificationCodeTwo.text.toString(),
+                binding.editTextFindIdVerificationCodeThree.text.toString(),
+                binding.editTextFindIdVerificationCodeFour.text.toString(),
+                FindIdCustomDialog(requireContext()),
+                binding.textViewFindIdNotCorrectEmailFormat,
+                binding.buttonFindIdVerificationCode,
+            )
+        }
+
         return binding.root
     }
 
-    private fun moveNextCodeBox(){
+
+    private fun nextToVerificationCodeBox(){
         with(binding){
             editTextFindIdVerificationCodeOne.addTextChangedListener{
-                if (it?.length!! == 1){
-                    editTextFindIdVerificationCodeTwo.requestFocus()
-                }
+                findIdViewModel.checkNumberInCodeBox(it?.length!!, numberBoxState, 0, editTextFindIdVerificationCodeTwo)
+            }
+            editTextFindIdVerificationCodeTwo.addTextChangedListener {
+                findIdViewModel.checkNumberInCodeBox(it?.length!!, numberBoxState, 1, editTextFindIdVerificationCodeThree)
+            }
+            editTextFindIdVerificationCodeThree.addTextChangedListener {
+                findIdViewModel.checkNumberInCodeBox(it?.length!!, numberBoxState, 2, editTextFindIdVerificationCodeFour)
+            }
+            editTextFindIdVerificationCodeFour.addTextChangedListener {
+                findIdViewModel.checkNumberInCodeBox(it?.length!!, numberBoxState, 3, null)
             }
         }
     }
+
+    private fun onActiveButton(value: Boolean){
+        binding.buttonFindIdVerificationCode.isEnabled = value
+    }
+
+    private fun observeData(){
+        findIdViewModel.numberBoxUiState.observe(viewLifecycleOwner, Observer<MutableList<Boolean>>{
+            var checkNumber = 0
+            it.forEach { boolData ->
+                if (!boolData) onActiveButton(boolData) else checkNumber ++
+            }
+            if (checkNumber == 4) onActiveButton(true)
+        })
+    }
+
 }
