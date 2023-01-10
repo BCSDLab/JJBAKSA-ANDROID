42930eda60a20e75a590bb7db883934cec75aa25
Jokwanhee <jonotch1@naver.com>
2023-01-10 20:28:30 +0900

    #25 [mod] modify find_id fragment

--- app/src/main/java/com/jjbaksa/jjbaksa/ui/findid/FindIdActivity.kt
+++ app/src/main/java/com/jjbaksa/jjbaksa/ui/findid/FindIdActivity.kt
@@ -1,14 +1,17 @@
 package com.jjbaksa.jjbaksa.ui.findid
 
+import androidx.activity.viewModels
 import com.jjbaksa.jjbaksa.R
 import com.jjbaksa.jjbaksa.base.BaseActivity
 import com.jjbaksa.jjbaksa.databinding.ActivityFindIdBinding
+import com.jjbaksa.jjbaksa.viewmodel.FindIdViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class FindIdActivity : BaseActivity<ActivityFindIdBinding>() {
     override val layoutId: Int
         get() = R.layout.activity_find_id
+
 
     override fun initView() {
         binding.lifecycleOwner = this
--- app/src/main/java/com/jjbaksa/jjbaksa/ui/findid/FindIdFragment.kt
+++ app/src/main/java/com/jjbaksa/jjbaksa/ui/findid/FindIdFragment.kt
@@ -1,38 +1,45 @@
 package com.jjbaksa.jjbaksa.ui.findid
 
 import android.os.Bundle
-import android.text.Editable
-import android.text.TextWatcher
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.core.widget.addTextChangedListener
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
 import com.jjbaksa.jjbaksa.R
 import com.jjbaksa.jjbaksa.databinding.FragmentFindIdBinding
-import java.util.regex.Pattern
+import com.jjbaksa.jjbaksa.viewmodel.FindIdViewModel
 
-class FindIdFragment: Fragment() {
+
+class FindIdFragment : Fragment() {
     private lateinit var binding: FragmentFindIdBinding
+
+    private val findIdViewModel: FindIdViewModel by activityViewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = DataBindingUtil.inflate(layoutInflater, R.layout.fragment_find_id, container, false)
+        binding =
+            DataBindingUtil.inflate(layoutInflater, R.layout.fragment_find_id, container, false)
 
+        binding.editTextFindIdToEmail.addTextChangedListener {
+            binding.buttonFindIdSendToVerificationCode.isEnabled = findIdViewModel.stateButton(it?.length!!)
+        }
 
-        inputEmailAddress()
         binding.buttonFindIdSendToVerificationCode.setOnClickListener {
-            if (checkEmailFormat()){
+            if (findIdViewModel.checkEmailFormat(binding.editTextFindIdToEmail.text.toString())) {
                 // Email format is OK
-//                binding.editTextFindIdToEmail.setText(null)
-                findNavController().navigate(R.id.action_find_id_to_input_id_verification_code)
+                findIdViewModel.getFindIdNumberCode(
+                    binding.editTextFindIdToEmail.text.toString(),
+                    findNavController(),
+                    R.id.action_find_id_to_input_id_verification_code
+                )
+
             } else {
                 // Email format is Fail
                 binding.textViewFindIdNotCorrectEmailFormat.visibility = View.VISIBLE
@@ -44,25 +51,12 @@ class FindIdFragment: Fragment() {
 
     override fun onStart() {
         super.onStart()
-        with(binding){
+        with(binding) {
             textViewFindIdNotCorrectEmailFormat.visibility = View.INVISIBLE
             buttonFindIdSendToVerificationCode.isEnabled = false
             editTextFindIdToEmail.setText(null)
         }
     }
 
-    private fun inputEmailAddress(){
-        binding.editTextFindIdToEmail.addTextChangedListener {
-            if (it?.length!! > 0){
-                binding.buttonFindIdSendToVerificationCode.isEnabled = true
-            }
-        }
-    }
-
-    private fun checkEmailFormat(): Boolean{
-        val emailValidation = "^[_A-Za-z0-9-]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$"
-
-        var email = binding.editTextFindIdToEmail.text.toString().trim()
-        return Pattern.matches(emailValidation, email)
-    }
 }
+
