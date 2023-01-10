d96ea203a5c4968142d3ebcbcaba552466b4c93e
Jokwanhee <jonotch1@naver.com>
2023-01-10 20:32:07 +0900

    #25 [add] find account api response value

---
+++ domain/src/main/java/com/jjbaksa/domain/resp/findid/FindIdResp.kt
@@ -0,0 +1,12 @@
+package com.jjbaksa.domain.resp.findid
+
+data class FindIdResp (
+    val account: String,
+    val email: String,
+    val id: Int,
+    val nickname: String,
+    val oauthType: String,
+    val profileImage: Map<String, Any>,
+    val userCountResponse: Map<String, Int>,
+    val userType: String
+)
