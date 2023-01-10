5b8be57fb7f6968db1f2eb35c3642cfa3229c21a
Jokwanhee <jonotch1@naver.com>
2023-01-10 20:31:28 +0900

    #25 [add] user email check api and find account api

--- data/src/main/java/com/jjbaksa/data/api/AuthApi.kt
+++ data/src/main/java/com/jjbaksa/data/api/AuthApi.kt
@@ -1,3 +1,21 @@
 package com.jjbaksa.data.api
 
-interface AuthApi
+import com.jjbaksa.domain.resp.findid.FindIdResp
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.Query
+
+interface AuthApi{
+    @POST("user/email")
+    fun getFindIdCodeNumber(
+        @Query("email") userEmail: String
+    ): Call<Unit>
+
+    @GET("user/account")
+    fun findAccount(
+        @Query("email") userEmail: String,
+        @Query("code") codeNumber: String
+    ): Call<FindIdResp>
+}
+
