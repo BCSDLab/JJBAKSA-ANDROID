9424914589f16d7b7086e505fa099f4f5519ab80
Jokwanhee <jonotch1@naver.com>
2023-01-26 01:27:59 +0900

    #35 [ui] add mainpage fragment xml

--- app/src/main/java/com/jjbaksa/jjbaksa/ui/mainpage/NaviHomeFragment.kt
+++ app/src/main/java/com/jjbaksa/jjbaksa/ui/mainpage/NaviHomeFragment.kt
@@ -25,7 +25,6 @@ class NaviHomeFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
     }
 
-
     companion object {
         fun newInstance(): NaviHomeFragment {
             val args = Bundle().apply {
