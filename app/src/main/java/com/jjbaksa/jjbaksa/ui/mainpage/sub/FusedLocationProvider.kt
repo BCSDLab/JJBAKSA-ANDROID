459a1fe6261bb688d222c006bebd3308b692ab71
Jokwanhee <jonotch1@naver.com>
2023-02-09 17:11:30 +0900

    #35 [add] Add real time user location

---
+++ app/src/main/java/com/jjbaksa/jjbaksa/ui/mainpage/sub/FusedLocationProvider.kt
@@ -0,0 +1,103 @@
+package com.jjbaksa.jjbaksa.ui.mainpage.sub
+
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
+import android.location.Location
+import android.os.Looper
+import android.util.Log
+import androidx.activity.viewModels
+import androidx.core.app.ActivityCompat
+import com.google.android.gms.location.*
+import com.jjbaksa.domain.model.mainpage.UserLocation
+import com.jjbaksa.jjbaksa.ui.mainpage.viewmodel.HomeViewModel
+
+class FusedLocationProvider(
+    private val context: Context,
+    private val viewModel: HomeViewModel
+) {
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+    private lateinit var locationCallback: LocationCallback
+
+    init {
+        initLocationClient()
+        initLocationCallback()
+    }
+
+    private fun initLocationClient() {
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(context)
+
+        val locationRequest = LocationRequest.create().apply {
+            interval = 1000
+            priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+        }
+
+        val builder = LocationSettingsRequest.Builder()
+            .addLocationRequest(locationRequest)
+        val client = LocationServices.getSettingsClient(context)
+        val task = client.checkLocationSettings(builder.build())
+        task.addOnSuccessListener {
+            // location client setting success
+        }
+
+        task.addOnFailureListener {
+            // location client setting failure
+        }
+    }
+
+    private fun initLocationCallback() {
+        locationCallback = object : LocationCallback() {
+            override fun onLocationResult(locationResult: LocationResult) {
+                super.onLocationResult(locationResult)
+                locationResult.locations.forEach { location ->
+                    // location call back
+                    viewModel.getLocation(location.latitude, location.longitude)
+                }
+            }
+        }
+    }
+
+    fun startLocationUpdates() {
+        if (ActivityCompat.checkSelfPermission(
+                context,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                context,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            return
+        }
+        val locationRequest = LocationRequest.create().apply {
+            interval = 1000
+            priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+        }
+        fusedLocationProviderClient.requestLocationUpdates(
+            locationRequest,
+            locationCallback,
+            Looper.getMainLooper()
+        )
+    }
+
+    fun requestLastLocation() {
+        if (ActivityCompat.checkSelfPermission(
+                context,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                context,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            return
+        }
+        fusedLocationProviderClient.lastLocation
+            .addOnSuccessListener { location: Location? ->
+                // response location
+                viewModel.getLocation(location?.latitude, location?.longitude)
+            }
+    }
+
+    fun stopLocationUpdates() {
+        fusedLocationProviderClient.removeLocationUpdates(locationCallback)
+    }
+}
