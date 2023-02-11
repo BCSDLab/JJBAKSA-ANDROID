e9b9faeeaa57e5ad6d2e7e5ca63bff44b4a98c85
Jokwanhee <jonotch1@naver.com>
2023-02-11 11:57:37 +0900

    #35 [add] ktlint check

--- app/src/main/java/com/jjbaksa/jjbaksa/ui/mainpage/sub/FusedLocationProvider.kt
+++ app/src/main/java/com/jjbaksa/jjbaksa/ui/mainpage/sub/FusedLocationProvider.kt
@@ -6,7 +6,12 @@ import android.content.pm.PackageManager
 import android.location.Location
 import android.os.Looper
 import androidx.core.app.ActivityCompat
-import com.google.android.gms.location.*
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.location.LocationSettingsRequest
+import com.google.android.gms.location.LocationResult
 import com.jjbaksa.jjbaksa.ui.mainpage.viewmodel.HomeViewModel
 
 class FusedLocationProvider(
@@ -59,9 +64,9 @@ class FusedLocationProvider(
                 context,
                 Manifest.permission.ACCESS_FINE_LOCATION
             ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
-                context,
-                Manifest.permission.ACCESS_COARSE_LOCATION
-            ) != PackageManager.PERMISSION_GRANTED
+                    context,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                ) != PackageManager.PERMISSION_GRANTED
         ) {
             return
         }
@@ -81,9 +86,9 @@ class FusedLocationProvider(
                 context,
                 Manifest.permission.ACCESS_FINE_LOCATION
             ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
-                context,
-                Manifest.permission.ACCESS_COARSE_LOCATION
-            ) != PackageManager.PERMISSION_GRANTED
+                    context,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                ) != PackageManager.PERMISSION_GRANTED
         ) {
             return
         }
