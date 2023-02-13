6b8b7571e0dcba78786449e0e68f102f3231b31e
Jokwanhee <jonotch1@naver.com>
2023-02-13 22:20:32 +0900

    #35 [add] Add action app details settings

--- app/src/main/java/com/jjbaksa/jjbaksa/ui/mainpage/sub/HomeAlertDialog.kt
+++ app/src/main/java/com/jjbaksa/jjbaksa/ui/mainpage/sub/HomeAlertDialog.kt
@@ -1,9 +1,12 @@
 package com.jjbaksa.jjbaksa.ui.mainpage.sub
 
 import android.app.Dialog
+import android.content.ContextWrapper
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.provider.Settings
+import android.util.Log
 import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.activityViewModels
@@ -11,14 +14,14 @@ import com.jjbaksa.jjbaksa.R
 import com.jjbaksa.jjbaksa.ui.mainpage.viewmodel.HomeViewModel
 
 class HomeAlertDialog() : DialogFragment() {
-    private val homeViewModel: HomeViewModel by activityViewModels()
-
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         return AlertDialog.Builder(requireContext()).apply {
             setMessage(R.string.location_service_term_text)
             setNegativeButton(R.string.cancel, null)
             setPositiveButton(R.string.move_setting) { _, _ ->
-                val intent = Intent(Settings.ACTION_SETTINGS)
+                val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS).apply {
+                    data = Uri.fromParts("package", context.packageName, null)
+                }
                 if (intent.resolveActivity(requireContext().packageManager) != null) {
                     startActivity(intent)
                 }
