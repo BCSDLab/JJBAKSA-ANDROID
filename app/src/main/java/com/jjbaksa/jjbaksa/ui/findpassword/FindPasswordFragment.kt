a91cdfca0fd6306618be15d27eeacddddc8a7392
Jokwanhee <jonotch1@naver.com>
2023-02-19 13:56:45 +0900

    #39 [ui] Add find password ui

---
+++ app/src/main/java/com/jjbaksa/jjbaksa/ui/findpassword/FindPasswordFragment.kt
@@ -0,0 +1,19 @@
+package com.jjbaksa.jjbaksa.ui.findpassword
+
+import com.jjbaksa.jjbaksa.R
+import com.jjbaksa.jjbaksa.base.BaseFragment
+import com.jjbaksa.jjbaksa.databinding.FragmentFindPasswordBinding
+
+class FindPasswordFragment():BaseFragment<FragmentFindPasswordBinding>() {
+    override val layoutId: Int
+        get() = R.layout.fragment_find_password
+
+    override fun initView() {
+    }
+
+    override fun initEvent() {
+    }
+
+    override fun subscribe() {
+    }
+}
---
+++ app/src/main/java/com/jjbaksa/jjbaksa/ui/findpassword/FindPasswordResetFragment.kt
@@ -0,0 +1,22 @@
+package com.jjbaksa.jjbaksa.ui.findpassword
+
+import com.jjbaksa.jjbaksa.R
+import com.jjbaksa.jjbaksa.base.BaseFragment
+import com.jjbaksa.jjbaksa.databinding.FragmentFindPasswordResetBinding
+
+class FindPasswordResetFragment: BaseFragment<FragmentFindPasswordResetBinding>() {
+    override val layoutId: Int
+        get() = R.layout.fragment_find_password_reset
+
+    override fun initView() {
+        TODO("Not yet implemented")
+    }
+
+    override fun initEvent() {
+        TODO("Not yet implemented")
+    }
+
+    override fun subscribe() {
+        TODO("Not yet implemented")
+    }
+}
