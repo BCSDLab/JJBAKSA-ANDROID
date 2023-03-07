608f1675bd66d2086a6be2e3cd4acd498bed9257
임성우 <seongwoorang@naver.com>
2023-03-07 13:17:45 +0900

    #36 [mod] modify permission code

--- app/src/main/java/com/jjbaksa/jjbaksa/ui/gallery/GalleryActivity.kt
+++ app/src/main/java/com/jjbaksa/jjbaksa/ui/gallery/GalleryActivity.kt
@@ -3,10 +3,9 @@ package com.jjbaksa.jjbaksa.ui.gallery
 import android.Manifest
 import android.content.Intent
 import android.os.Build
-import android.util.Log
+import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
-import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AlertDialog
 import androidx.recyclerview.widget.GridLayoutManager
 import com.jjbaksa.jjbaksa.R
@@ -24,9 +23,20 @@ class GalleryActivity : BaseActivity<ActivityGalleryBinding>() {
             when (isGranted) {
                 true -> getAllPhotos()
                 else -> {
-                    when (shouldShowRequestPermissionRationale(Manifest.permission.READ_EXTERNAL_STORAGE)) {
-                        true -> permissionDialog(true)
-                        else -> permissionDialog(false)
+                    Toast.makeText(applicationContext, "권한 허용 필요", Toast.LENGTH_SHORT).show()
+                    finish()
+                }
+            }
+        }
+
+    private val requestMultiplePermission =
+        registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { results ->
+            results.forEach {
+                when (it.value) {
+                    true -> getAllPhotos()
+                    else -> {
+                        Toast.makeText(applicationContext, "권한 허용 필요", Toast.LENGTH_SHORT).show()
+                        finish()
                     }
                 }
             }
@@ -56,7 +66,6 @@ class GalleryActivity : BaseActivity<ActivityGalleryBinding>() {
         }
     }
 
-    @RequiresApi(Build.VERSION_CODES.M)
     private fun permissionDialog(isDeniedOnce: Boolean) {
         val builder = AlertDialog.Builder(this)
         builder.setTitle("권한이 필요합니다.")
@@ -65,6 +74,7 @@ class GalleryActivity : BaseActivity<ActivityGalleryBinding>() {
                 if (isDeniedOnce) {
                     checkPermission()
                 } else {
+                    checkPermission()
                 }
             }
             .setNegativeButton("cancel") { dialog, _ ->
@@ -75,7 +85,14 @@ class GalleryActivity : BaseActivity<ActivityGalleryBinding>() {
     }
 
     private fun checkPermission() {
-        requestPermission.launch(Manifest.permission.READ_EXTERNAL_STORAGE)
+        val permissionList = arrayOf(
+            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            Manifest.permission.READ_EXTERNAL_STORAGE)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            requestPermission.launch(Manifest.permission.READ_EXTERNAL_STORAGE)
+        } else {
+            requestMultiplePermission.launch(permissionList)
+        }
     }
 
     override fun initView() {
