59f49d66f7826eb133b6856e23fc1744d446728a
임성우 <seongwoorang@naver.com>
2023-03-10 19:19:40 +0900

    #36 [del] delete unused import

--- app/src/main/java/com/jjbaksa/jjbaksa/ui/gallery/GalleryAdapter.kt
+++ app/src/main/java/com/jjbaksa/jjbaksa/ui/gallery/GalleryAdapter.kt
@@ -5,7 +5,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.LinearLayout
-import androidx.lifecycle.LifecycleOwner
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.jjbaksa.domain.model.Image
