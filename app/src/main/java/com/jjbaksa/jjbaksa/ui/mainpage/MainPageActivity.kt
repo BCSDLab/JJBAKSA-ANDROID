8748b13a28fdf5f69ecf74b1618c7193218f3303
JoKwanhee <jonotch1@naver.com>
2023-07-01 20:45:38 +0900

    #48-add-main-page-navigation

--- app/src/main/java/com/jjbaksa/jjbaksa/ui/mainpage/MainPageActivity.kt
+++ app/src/main/java/com/jjbaksa/jjbaksa/ui/mainpage/MainPageActivity.kt
@@ -1,9 +1,14 @@
 package com.jjbaksa.jjbaksa.ui.mainpage
 
 import android.Manifest
+import android.util.Log
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.onNavDestinationSelected
+import androidx.navigation.ui.setupWithNavController
 import com.jjbaksa.jjbaksa.R
 import com.jjbaksa.jjbaksa.base.BaseActivity
 import com.jjbaksa.jjbaksa.databinding.ActivityMainPageBinding
@@ -14,9 +19,6 @@ import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainPageActivity : BaseActivity<ActivityMainPageBinding>() {
-    private val naviFragmentHome by lazy { NaviHomeFragment.newInstance() }
-    private val naviFragmentWrite by lazy { NaviWriteFragment.newInstance() }
-    private val naviFragmentMyPage by lazy { NaviMyPageFragment.newInstance() }
     override val layoutId: Int
         get() = R.layout.activity_main_page
 
@@ -49,39 +51,18 @@ class MainPageActivity : BaseActivity<ActivityMainPageBinding>() {
         homeViewModel.fusedLocationProvider = FusedLocationProvider(this, homeViewModel)
     }
 
-    override fun subscribe() {
-    }
+    override fun subscribe() {}
 
     override fun initEvent() {
-        initNavigationBar()
+        initNavigation()
         checkLocationPermissions()
     }
 
-    private fun initNavigationBar() {
-        binding.navigationView.run {
-            setOnItemSelectedListener {
-                when (it.itemId) {
-                    R.id.homeItem -> {
-                        changeFragment(naviFragmentHome)
-                    }
-                    R.id.writeItem -> {
-                        changeFragment(naviFragmentWrite)
-                    }
-                    R.id.myPageItem -> {
-                        changeFragment(naviFragmentMyPage)
-                    }
-                }
-                true
-            }
-            selectedItemId = R.id.homeItem
-        }
-    }
-
-    private fun changeFragment(fragment: Fragment) {
-        supportFragmentManager
-            .beginTransaction()
-            .replace(R.id.fragment_container_view, fragment)
-            .commit()
+    private fun initNavigation() {
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragment_container_view) as NavHostFragment
+        val navController = navHostFragment.findNavController()
+        binding.navigationView.setupWithNavController(navController)
     }
 
     private fun checkLocationPermissions() {
