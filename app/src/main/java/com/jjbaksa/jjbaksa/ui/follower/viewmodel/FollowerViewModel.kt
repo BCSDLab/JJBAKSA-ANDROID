a5f6d8dcf6c08e1a6bfa73b51835cc8a76358076
nazeroo <skdud4081@gmail.com>
2024-01-25 10:58:45 +0900

    #147 add get follower function at viewmodel

--- app/src/main/java/com/jjbaksa/jjbaksa/ui/follower/viewmodel/FollowerViewModel.kt
+++ app/src/main/java/com/jjbaksa/jjbaksa/ui/follower/viewmodel/FollowerViewModel.kt
@@ -1,7 +1,9 @@
 package com.jjbaksa.jjbaksa.ui.follower.viewmodel
 
+import android.util.Log
 import androidx.lifecycle.viewModelScope
 import com.jjbaksa.domain.enums.InquiryCursor
+import com.jjbaksa.domain.model.follower.Follower
 import com.jjbaksa.domain.model.inquiry.Inquiry
 import com.jjbaksa.domain.usecase.follower.FollowerUseCase
 import com.jjbaksa.domain.usecase.inquiry.InquiryUseCase
@@ -15,5 +17,22 @@ import javax.inject.Inject
 class FollowerViewModel @Inject constructor(
     private val followerUseCase: FollowerUseCase
 ) : BaseViewModel() {
+    private val _followerList = SingleLiveEvent<Follower>()
+    val followerList: SingleLiveEvent<Follower> get() = _followerList
+    val followerHasMore = SingleLiveEvent<Boolean>()
+
+    fun getFollower(cursor: String?, pageSize: Int) {
+        viewModelScope.launch(ceh) {
+            followerUseCase.getFollower(cursor, pageSize).collect {
+                it.onSuccess {
+
+                    followerList.value = it
+                    followerHasMore.value = it.content.count() == 10
+                }
+
+            }
+        }
+    }
+
 
 }
