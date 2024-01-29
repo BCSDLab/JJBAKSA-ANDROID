6cda38248d39d6936714f0d942ebbb1ce6fe8dcf
Jokwanhee <jonotch1@naver.com>
2024-01-29 18:00:39 +0900

    [mod] #151 api request header method

--- data/src/main/java/com/jjbaksa/data/api/NoAuthApi.kt
+++ data/src/main/java/com/jjbaksa/data/api/NoAuthApi.kt
@@ -23,7 +23,7 @@ import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface NoAuthApi {
-    @GET("login/{sns-type}")
+    @POST("login/{sns-type}")
     suspend fun postLoginSNS(
         @Header("Authorization") token: String,
         @Path("sns-type") snsType: String
--- data/src/main/java/com/jjbaksa/data/api/RefreshApi.kt
+++ data/src/main/java/com/jjbaksa/data/api/RefreshApi.kt
@@ -1,9 +1,10 @@
 package com.jjbaksa.data.api
 import com.jjbaksa.data.model.user.LoginResp
 import retrofit2.Response
+import retrofit2.http.GET
 import retrofit2.http.POST
 
 interface RefreshApi {
-    @POST("user/refresh")
+    @GET("user/refresh")
     suspend fun getRefreshAuth(): Response<LoginResp>
 }
