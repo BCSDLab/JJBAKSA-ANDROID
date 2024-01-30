af540abee7da94a1b89da61cf3bf53cacdc3b11a
nazeroo <skdud4081@gmail.com>
2024-01-30 10:24:22 +0900

    #147 refector followers class name

--- data/src/main/java/com/jjbaksa/data/model/follower/FollowersListResp.kt
+++ data/src/main/java/com/jjbaksa/data/model/follower/FollowersListResp.kt
@@ -2,7 +2,7 @@ package com.jjbaksa.data.model.follower
 
 import com.google.gson.annotations.SerializedName
 
-data class FollowerslistResp(
+data class FollowersListResp(
     @SerializedName("content")
     val content: List<FollowContentResp> = listOf(),
 
