14965c164ea871eff48a874dd6ac9b885e20d20a
Strone <sangbbang31@gmail.com>
2024-01-31 05:21:15 +0900

    #161 fix recyclerview same data bug

--- app/src/main/java/com/jjbaksa/jjbaksa/ui/mainpage/mypage/adapter/ReviewDetailAdapter.kt
+++ app/src/main/java/com/jjbaksa/jjbaksa/ui/mainpage/mypage/adapter/ReviewDetailAdapter.kt
@@ -1,5 +1,6 @@
 package com.jjbaksa.jjbaksa.ui.mainpage.mypage.adapter
 
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
