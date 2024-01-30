2996a8ab6ff07d9116ad48feb22e1b81eb1b577a
Jokwanhee <jonotch1@naver.com>
2024-01-31 01:49:31 +0900

    [mod] #159 shop & scrap dto

--- data/src/main/java/com/jjbaksa/data/model/shop/ShopsMapsResp.kt
+++ data/src/main/java/com/jjbaksa/data/model/shop/ShopsMapsResp.kt
@@ -1,6 +1,8 @@
 package com.jjbaksa.data.model.shop
 
 import com.google.gson.annotations.SerializedName
+import com.jjbaksa.data.model.CoordinateDto
+import com.jjbaksa.data.model.pin.RateDto
 import com.jjbaksa.domain.ErrorResp
 
 data class ShopsMapsResp(
@@ -8,8 +10,18 @@ data class ShopsMapsResp(
     val placeId: String? = "",
     @SerializedName("name")
     val name: String? = "",
-    @SerializedName("geometry")
-    val geometry: GeometryDTO,
-    @SerializedName("photo")
-    val photo: String? = ""
+    @SerializedName("coordinate")
+    val coordinate: CoordinateDto,
+    @SerializedName("category")
+    val category: String? = "",
+    @SerializedName("photos")
+    val photo: List<String>? = emptyList(),
+    @SerializedName("rate")
+    val rate: RateDto? = RateDto(),
+    @SerializedName("openNow")
+    val openNow: Boolean? = false,
+    @SerializedName("formattedAddress")
+    val formattedAddress: String? = "",
+    @SerializedName("simpleFormattedAddress")
+    val simpleFormattedAddress: String? = ""
 ) : ErrorResp()
