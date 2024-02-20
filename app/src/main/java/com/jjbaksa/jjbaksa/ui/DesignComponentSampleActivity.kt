16b5676e5118387698a0c87ee245e264a673f271
Jokwanhee <jonotch1@naver.com>
2024-02-20 14:18:57 +0900

    #168 add Design component sample activity

---
+++ app/src/main/java/com/jjbaksa/jjbaksa/ui/DesignComponentSampleActivity.kt
@@ -0,0 +1,91 @@
+package com.jjbaksa.jjbaksa.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.android.jj_design.utils.button.Add
+import com.android.jj_design.utils.button.Confirm
+import com.android.jj_design.utils.button.Delete
+import com.android.jj_design.utils.button.Follow
+import com.android.jj_design.utils.button.Following
+import com.android.jj_design.utils.button.Minus
+import com.android.jj_design.utils.button.Position
+import com.android.jj_design.utils.button.Requested
+import com.jjbaksa.jjbaksa.databinding.ActivityDesignComponentSampleBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.isActive
+import kotlinx.coroutines.launch
+
+class DesignComponentSampleActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityDesignComponentSampleBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityDesignComponentSampleBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initJjButton()
+        initJjChipButton()
+        initJjTrendButton()
+        initJjMapButton()
+    }
+
+    private fun initJjButton() {
+        CoroutineScope(Dispatchers.Main).launch {
+            while (this.isActive) {
+                binding.jjButton.setErrorBackground()
+                binding.jjButton.text = "오류 버튼"
+                delay(2000)
+                binding.jjButton.isEnabled = true
+                binding.jjButton.text = "활성화 버튼"
+                delay(2000)
+                binding.jjButton.isEnabled = false
+                binding.jjButton.text = "비활성화 버튼"
+                delay(2000)
+            }
+        }
+    }
+
+    private fun initJjChipButton() {
+        CoroutineScope(Dispatchers.Main).launch {
+            while (isActive) {
+                binding.jjChipButton.setChipItemType(Delete)
+                delay(2000)
+                binding.jjChipButton.setChipItemType(Confirm)
+                delay(2000)
+                binding.jjChipButton.setChipItemType(Requested)
+                delay(2000)
+                binding.jjChipButton.setChipItemType(Follow)
+                delay(2000)
+                binding.jjChipButton.setChipItemType(Following)
+                delay(2000)
+            }
+        }
+    }
+
+    private fun initJjTrendButton() {
+        CoroutineScope(Dispatchers.Main).launch {
+            while (isActive) {
+                binding.jjTrendButton.isEnabled = true
+                binding.jjTrendButton.text = "#성수동 맛집"
+                delay(2000)
+                binding.jjTrendButton.isEnabled = false
+                binding.jjTrendButton.text = "#카페 마이야르"
+                delay(2000)
+            }
+        }
+    }
+
+    private fun initJjMapButton() {
+        CoroutineScope(Dispatchers.Main).launch {
+            while (isActive) {
+                binding.jjMapButton.setJjMapType(Add)
+                delay(2000)
+                binding.jjMapButton.setJjMapType(Minus)
+                delay(2000)
+                binding.jjMapButton.setJjMapType(Position)
+                delay(2000)
+            }
+        }
+    }
+}
